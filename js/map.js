5fd5a1a732ae2aaa02e6afefc796430984b411bb
ioyli <90900819+ioyli@users.noreply.github.com>
2022-11-15 12:54:39 +0200

    Scrolling background

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -9,6 +10,7 @@ document.getElementById('startBtn').addEventListener('click', start)
 
 function start() {
     showGame()
+    animate()
 }
 
 function showGame() {
@@ -31,4 +33,38 @@ function hideEnd() {
 
 function hideStart() {
     startEle.style.display = 'none'
+}
+
+class Background {
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth
+        this.gameHeight = gameHeight
+        this.image = document.getElementById('backgroundImage')
+        this.x = 0
+        this.y = 0
+        this.width = 1600
+        this.height = 800
+        this.speed = 4
+    }
+    draw(context) {
+        context.drawImage(this.image, this.x, this.y, this.width, this.height)
+        context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height)
+    }
+    update() {
+        this.x -= this.speed
+        if (this.x < 0 - this.width) this.x = 0
+    }
+}
+
+class Obstacles {
+    
+}
+
+const background = new Background(canvas.width, canvas.height)
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    background.draw(ctx)
+    background.update()
+    requestAnimationFrame(animate)
 }
